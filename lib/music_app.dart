43c4cf5c50290129935ea195f19a75997b8f995e
Kossi ADANOU <kjadanou@gmail.com>
2024-01-19 00:00:52 +0000

    updated home and entry pages

--- lib/music_app.dart
+++ lib/music_app.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:music_app/home_page.dart';
-import 'package:music_app/music_list_page.dart';
 
 class MusicApp extends StatelessWidget {
   const MusicApp({super.key});
@@ -15,7 +14,7 @@ class MusicApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MusicListPage(),
+      home: const HomePage(),
     );
   }
 }
